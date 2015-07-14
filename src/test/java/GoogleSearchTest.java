2b36f14caaf83a961c5980117d7fb36953e15fa3
Ilir Kosumi <ikosumi@lixar.com>
2015-07-14 13:20:06 +0200

    fixed checking for correct tile by using implicit wait

--- src/test/java/GoogleSearchTest.java
+++ src/test/java/GoogleSearchTest.java
@@ -2,6 +2,12 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
 import static org.testng.Assert.*;
 
 public class GoogleSearchTest {
@@ -15,10 +21,14 @@ public class GoogleSearchTest {
 	public static final String WIKIPEDIA_CSS_SELECTOR = "h3.r a";
 	public static final String GOOGLE_SIDEBAR_TITLE = "Google";
 	public static final String GOOGLE_SIDEBAR_TITLE_CSS_SELECTOR = "div.kno-ecr-pt";
+	public static final int IMPLICIT_TIMEOUT_IN_SECONDS = 10;
 
-	public static void main(String[] args) {
+	@Test
+	public void googleGoole() {
 		//create a Firefox instance of the WebDriver interface
 		WebDriver ffDriver = new FirefoxDriver();
+		//managing timeouts
+		ffDriver.manage().timeouts().implicitlyWait(IMPLICIT_TIMEOUT_IN_SECONDS, TimeUnit.SECONDS);
 		//navigate to Google
 		ffDriver.get(GOOGLE_HOMEPAGE);
 		//get a reference to the search box
@@ -27,12 +37,22 @@ public class GoogleSearchTest {
 		searchTxtBox.sendKeys(MISSPELLED_SEARCH);
 		//pretty much self-explanatory :)
 		searchTxtBox.submit();
-		//check for window title
-		assertTrue(ffDriver.getTitle().equals(BROWSER_TITLE));
-		//check for spelling
-		assertEquals((ffDriver.findElement(By.cssSelector(GOOGLE_SPELLING_SUGGESTION_CSS_SELECTOR)).getText()), CORRECT_SPELLED_SEARCH);
+
+		//check for browser window
+		Boolean isCorrectbrowserTitle = (new WebDriverWait(ffDriver, IMPLICIT_TIMEOUT_IN_SECONDS)).until(new ExpectedCondition<Boolean>() {
+			public Boolean apply(WebDriver driver) {
+				return driver.getTitle().equalsIgnoreCase(BROWSER_TITLE);
+			}
+		});
+		if (isCorrectbrowserTitle) {
+			assertEquals(ffDriver.getTitle(), BROWSER_TITLE);
+		} else {
+			fail("The browser title was incorrect.");
+		}
+		//check spelling
+		assertEquals(ffDriver.findElement(By.cssSelector(GOOGLE_SPELLING_SUGGESTION_CSS_SELECTOR)).getText(), CORRECT_SPELLED_SEARCH);
 		//Check wikipedia link which is the first hit
-		assertEquals(ffDriver.findElements(By.cssSelector(WIKIPEDIA_CSS_SELECTOR)).get(1).getText(), WIKIPEDIA_HIT);
+		assertEquals(ffDriver.findElements(By.cssSelector(WIKIPEDIA_CSS_SELECTOR)).get(0).getText(), WIKIPEDIA_HIT);
 		//Check right sidebar for a brief description of Google
 		assertEquals(ffDriver.findElement(By.cssSelector(GOOGLE_SIDEBAR_TITLE_CSS_SELECTOR)).getText(), GOOGLE_SIDEBAR_TITLE);
 		//quit browser session
